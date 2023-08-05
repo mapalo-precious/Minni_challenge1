a09ee9d287d7edc8f4a1e6f2e2c6184e735c7eb2
Precious Mapalo Nayingwa <Preciousnayingwa@gmail.com>
2023-08-05 22:15:39 +1100

    first commit

---
+++ Minni_challenge 1.py
@@ -0,0 +1,28 @@
+
+#Mini Challenge 1
+#Write a code that takes in 2 lists x = [-1 -4 -7] and y = [-3 7 4] and calculates the absolute sum
+
+def my_function():
+    x=[-1,-4,-7]
+    y=[-3,7,4]
+    z=sum(x+y)
+    return z
+
+print(my_function())
+
+#Mini Challenge 2
+#Write a code that takes in two inputs (number of items bought and prices) from the user and calculate the total account balance
+def total_():
+    items=input("How many items did you buy?")
+    items=int()
+    prices=input("What are the prices of those items")
+    prices=list()
+    sum_=sum(prices)
+    return sum_
+
+print(total_())
+
+#Mini challenge 3
+#Repeat mini challenge 2 (Write a code that takes in two inputs from the user and calculates the total) using lambda expressions instead
+
+#Mini challenge 4
