f295e1d7c3b7aa434875119fdc49af4f99eaea13
Precious Mapalo Nayingwa <Preciousnayingwa@gmail.com>
2023-08-06 14:39:28 +1100

    first commit

--- Minni_challenge 1.py
+++ Minni_challenge 1.py
@@ -15,14 +15,17 @@ print(my_function())
 def total_():
     items=input("How many items did you buy?")
     items=int()
-    prices=input("What are the prices of those items")
-    prices=list()
-    sum_=sum(prices)
-    return sum_
+    input_prices=input("What are the prices of those items(NOTE: use space to separate the prices)  ").split()
+    prices=[int(item) for item in input_prices]
+    print(prices)
+    return sum(prices)
 
-print(total_())
+total_()
+#
+# print(total_())
 
 #Mini challenge 3
-#Repeat mini challenge 2 (Write a code that takes in two inputs from the user and calculates the total) using lambda expressions instead
+#Repeat mini challenge 2 (Write a code that takes in two inputs from the user and calculates the total) using lambda expressions insteadit
+total= lambda x= input('how many items did you buy'), y= input('what are the prices os those items(NOTE: use space to separate the prices)').split : [int(item) for item in y], sum(y)
 
 #Mini challenge 4
